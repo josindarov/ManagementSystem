4e7eb7cb288521c42365715f8b8bc9c2f0f34268
Jurabek <jorabeksindarov44@gmail.com>
2024-02-25 17:12:41 +0500

    ShouldThrowValidationExceptionOnRemoveIfStorageAssignmentIsInvalidAndLogItAsync -> FAIL

--- ManagementSystem.UnitTest/Services/Foundation/Assignments/AssignmentServiceTests.Validation.Remove.cs
+++ ManagementSystem.UnitTest/Services/Foundation/Assignments/AssignmentServiceTests.Validation.Remove.cs
@@ -50,4 +50,50 @@ public partial class AssignmentServiceTests
         this.storageBrokerMock.VerifyNoOtherCalls();
         this.dateTimeBrokerMock.VerifyNoOtherCalls();
     }    
+    
+    [Fact]
+    public async Task ShouldThrowValidationExceptionOnRemoveIfStorageAssignmentIsInvalidAndLogItAsync()
+    {
+        // given
+        DateTimeOffset dateTimeOffset = GetRandomDateTime();
+        Assignment randomAssignment = CreateRandomAssignment(dates: dateTimeOffset);
+        Guid inputAssignmentId = randomAssignment.Id;
+        Assignment inputAssignment = randomAssignment;
+        Assignment nullStorageAssignment = null;
+
+        var notFoundAssignmentException = new NotFoundAssignmentException(inputAssignmentId);
+
+        var expectedAssignmentValidationException =
+            new AssignmentValidationException(notFoundAssignmentException);
+
+        this.storageBrokerMock.Setup(broker =>
+                broker.SelectAssignmentsByIdAsync(inputAssignmentId))!
+            .ReturnsAsync(nullStorageAssignment);
+
+        // when
+        ValueTask<Assignment> actualAssignmentTask =
+            this.assignmentService.RemoveAssignmentAsync(inputAssignmentId);
+
+        AssignmentValidationException actualAssignmentValidationException = await Assert
+            .ThrowsAsync<AssignmentValidationException>(() => actualAssignmentTask.AsTask());
+        
+        // then
+        actualAssignmentValidationException.Should().BeEquivalentTo(expectedAssignmentValidationException);
+        
+        this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedAssignmentValidationException))),
+            Times.Once);
+
+        this.storageBrokerMock.Verify(broker =>
+                broker.SelectAssignmentsByIdAsync(inputAssignmentId),
+            Times.Once);
+
+        this.storageBrokerMock.Verify(broker =>
+                broker.DeleteAssignmentsAsync(It.IsAny<Assignment>()),
+            Times.Never);
+
+        this.loggingBrokerMock.VerifyNoOtherCalls();
+        this.storageBrokerMock.VerifyNoOtherCalls();
+        this.dateTimeBrokerMock.VerifyNoOtherCalls();
+    }
 }
